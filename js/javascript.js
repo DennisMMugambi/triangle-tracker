aa5364a9429a27f5818b65ce3288621d807c8a52
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2019-02-15 16:10:03 +0300

    replaced parseInt with parseFloat

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,10 @@
 function triangleChecker(){
   var height = document.getElementById("length1").value;
-  var newHeight = parseInt(height);
+  var newHeight = parseFloat(height);
   var length = document.getElementById("length2").value;
-  var newLength = parseInt(length);
+  var newLength = parseFloat(length);
   var width = document.getElementById("length3").value;
-  var newWidth = parseInt(width);
+  var newWidth = parseFloat(width);
   var triangle = [];
 
   triangle = [newHeight,newLength,newWidth];
