bb8156c4c892617f4a5e29f25766e6b3bb4b737a
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2019-02-15 12:09:40 +0300

    added new if statement to prompt user to only enter non-negative values

--- js/javascript.js
+++ js/javascript.js
@@ -6,13 +6,18 @@ function triangleChecker(){
   var width = document.getElementById("length3").value;
   var newWidth = parseInt(width);
   var triangle = [];
+
   triangle = [newHeight,newLength,newWidth];
 
-  if ((triangle[0] + triangle[1]) <= triangle[3] || (triangle[0] + triangle[2] <= triangle[1])){
-    alert("a triangle cannot be formed");
-  } else if ((triangle[1] + triangle[2]) <= triangle[0]){
-    alert("a triangle cannot be formed");
+  if((triangle[0] > 0) && (triangle[1] > 0) && (triangle[2] > 0)){
+    if ((triangle[0] + triangle[1]) <= triangle[3] || (triangle[0] + triangle[2] <= triangle[1])){
+      alert("a triangle cannot be formed");
+    } else if ((triangle[1] + triangle[2]) <= triangle[0]){
+      alert("a triangle cannot be formed");
+    } else {
+      alert("a triangle can be formed");
+    }
   } else {
-    alert("a triangle can be formed");
+  alert("kindly enter values above zero");
   }
 }
