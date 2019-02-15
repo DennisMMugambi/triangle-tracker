b7dca94c70fbe238d08f62df942bf8609816e8d1
Dennis Mugambi <dennis.mutwiri.mugambi@gmail.com>
2019-02-15 09:41:21 +0300

    added length variables to javascript.js

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+function triangleChecker (){
+  var lengths = [];
+  var length1 = document.getElementById("length1");
+  var length2 = document.getElementById("length2");
+  var length3 = document.getElementById("lenght3");
+
+}
